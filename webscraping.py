48e1fe401f779ad212f06e4cf049c8cd7feace46
nimpy-wth <nimmyliala@gmail.com>
2025-07-23 15:57:51 +0700

    fill search

---
+++ webscraping.py
@@ -0,0 +1,16 @@
+import asyncio
+from playwright.async_api import async_playwright
+
+async def scrape_program_links(query="วิศวกรรมคอมพิวเตอร์"):
+    async with async_playwright() as p:
+        browser = await p.chromium.launch(headless=False) 
+        page = await browser.new_page()
+        await page.goto("https://course.mytcas.com", timeout=60000)
+
+        await page.wait_for_selector('input#search')
+        await page.fill('input#search', query)
+        await page.keyboard.press('Enter')
+        await browser.close()
+
+if __name__ == "__main__":
+    asyncio.run(scrape_program_links())
