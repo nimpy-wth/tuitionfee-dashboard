65272c5bd42be00273269fd9267d5ed415a423fb
nimpy-wth <nimmyliala@gmail.com>
2025-07-24 14:49:55 +0700

    main function

--- webscraping.py
+++ webscraping.py
@@ -1,30 +1,28 @@
 import asyncio
 from playwright.async_api import async_playwright
 
-async def search_for_programs(query="วิศวกรรมปัญญาประดิษฐ์"):
-    async with async_playwright() as p:
-        browser = await p.chromium.launch(headless=False) 
-        page = await browser.new_page()
-        await page.goto("https://course.mytcas.com", timeout=60000)
+async def search_for_programs(page, query):
+    
+    await page.goto("https://course.mytcas.com", timeout=60000)
 
-        # search for the query
-        await page.wait_for_selector('input#search')
-        await page.fill('input#search', '')
-        await page.type('input#search', query, delay=100)
-        await page.keyboard.press('Enter')
-        await page.wait_for_selector('.t-programs li a', timeout=15000) 
+    # search for the query
+    await page.wait_for_selector('input#search')
+    await page.fill('input#search', '')
+    await page.type('input#search', query, delay=100)
+    await page.keyboard.press('Enter')
+    await page.wait_for_selector('.t-programs li a', timeout=15000) 
 
-        # extract results
-        links = await page.eval_on_selector_all(
-            ".t-programs li a",
-            "els => els.map(el => ({ title: el.innerText.trim(), url: el.href }))"
+    # extract results
+    links = await page.eval_on_selector_all(
+        ".t-programs li a",
+        "els => els.map(el => ({ title: el.innerText.trim(), url: el.href }))"
         )
         
-        print(f"Found {len(links)} results for: {query}")
-        return links
-        # for i, link in enumerate(links, 1):
-        #     print(f"{i:02d}. {link['title']} — {link['url']}")
-        # await browser.close()
+    print(f"Found {len(links)} results for: {query}")
+    return links
+    # for i, link in enumerate(links, 1):
+    #     print(f"{i:02d}. {link['title']} — {link['url']}")
+    # await browser.close()
 
 async def scrape_details(context, program_info):
 
@@ -36,5 +34,22 @@ async def scrape_details(context, program_info):
 
     await details_page.close()
 
+async def main(query):
+    async with async_playwright() as p:
+        browser = await p.chromium.launch(headless=False)
+        context = await browser.new_context()
+        page = await context.new_page()
+
+        # get all program links
+        program_links = await search_for_programs(page, query)
+        await page.close() 
+
+        # scrape each program's details
+        for link in program_links:
+            await scrape_details(context, link)
+
+        await context.close()
+        await browser.close()
+
 if __name__ == "__main__":
-    asyncio.run(search_for_programs())
+    asyncio.run(main(query="วิศวกรรมปัญญาประดิษฐ์"))
